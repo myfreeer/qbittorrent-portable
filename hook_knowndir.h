5203e3caaa3bc2238bcc68375b34fbb18e763fe2
myfreeer <myfreeer@users.noreply.github.com>
2018-05-13 09:52:39 +0800

    hook: add hook for SHGetSpecialFolderPath
    improves compatibility of older qt5

--- hook_knowndir.h
+++ hook_knowndir.h
@@ -3,14 +3,23 @@
 #include <stdbool.h>
 #include "minhook/include/MinHook.h"
 
-typedef HRESULT (WINAPI *pSHGetKnownFolderPath)(
-        _In_     REFKNOWNFOLDERID rfid,
-        _In_     DWORD            dwFlags,
-        _In_opt_ HANDLE           hToken,
-        _Out_    PWSTR            *ppszPath
-);
-
-pSHGetKnownFolderPath OriginalSHGetKnownFolderPath = NULL;
+#define CreateMinHook(func) MH_CreateHook(func, &Hook##func, (LPVOID *) &Original##func)
+#ifdef HookDebug
+#define EnableMinHook(func, status) if ((status) == MH_OK) { \
+    (status) = MH_EnableHook(func); \
+        if ((status) == MH_OK) { \
+            MessageBox(NULL,  (LPCTSTR) _T(#func " Hook Succeed"), (LPCTSTR) _T(#func " Hook"), MB_OK); \
+        } else {\
+            MessageBox(NULL,  (LPCTSTR) _T(#func " Hook Enable Fail"), (LPCTSTR) _T(#func " Hook"), MB_OK); \
+        }\
+    } else { \
+        MessageBox(NULL,  (LPCTSTR) _T(#func " Hook Fail"), (LPCTSTR) _T#func (" Hook"), MB_OK); \
+    }
+#else // HookDebug
+#define EnableMinHook(func, status) if ((status) == MH_OK) { \
+    (status) = MH_EnableHook(func); \
+    }
+#endif // HookDebug
 
 /**
  * GetModulePath
@@ -25,6 +34,15 @@ DWORD WINAPI GetModulePath(TCHAR *pDirBuf, DWORD bufSize) {
 	*(szEnd) = 0;
 	return szEnd - pDirBuf;
 }
+
+typedef HRESULT (WINAPI *pSHGetKnownFolderPath)(
+        _In_     REFKNOWNFOLDERID rfid,
+        _In_     DWORD            dwFlags,
+        _In_opt_ HANDLE           hToken,
+        _Out_    PWSTR            *ppszPath
+);
+
+pSHGetKnownFolderPath OriginalSHGetKnownFolderPath = NULL;
 
 HRESULT WINAPI HookSHGetKnownFolderPath(
         _In_     REFKNOWNFOLDERID rfid,
@@ -50,29 +68,50 @@ HRESULT WINAPI HookSHGetKnownFolderPath(
     } else return OriginalSHGetKnownFolderPath(rfid, dwFlags, hToken, ppszPath);
 }
 
+typedef WINBOOL (WINAPI *pSHGetSpecialFolderPath) (
+        HWND   hwndOwner,
+        _Out_ LPTSTR lpszPath,
+        _In_  int    csidl,
+        _In_  BOOL   fCreate
+);
+
+pSHGetSpecialFolderPath OriginalSHGetSpecialFolderPath = NULL;
+
+WINBOOL WINAPI HookSHGetSpecialFolderPath(
+        HWND   hwndOwner,
+        _Out_ LPTSTR lpszPath,
+        _In_  int    csidl,
+        _In_  BOOL   fCreate
+) {
+    if (csidl == CSIDL_APPDATA ||
+        csidl == CSIDL_PROGRAMS ||
+        csidl == CSIDL_COMMON_APPDATA ||
+        csidl == CSIDL_LOCAL_APPDATA) {
+        if (lpszPath == NULL) {
+            return FALSE;
+        }
+        GetModulePath(lpszPath, MAX_PATH);
+#ifdef HookDebug
+        MessageBox(NULL, lpszPath, (LPCTSTR) _T("SHGetSpecialFolderPath Hook Path"), MB_OK);
+#endif
+        return S_OK;
+    } else return OriginalSHGetSpecialFolderPath(hwndOwner, lpszPath, csidl, fCreate);
+}
+
+
 void DLLHijackAttach(bool isSucceed) {
     if (isSucceed) {
+        MH_Initialize();
 #ifdef HookDebug
         MessageBox(NULL, TEXT("DLL Hijack Attach Succeed!"), TEXT(DLL_NAME " DLL Hijack Attach"), MB_OK);
 #endif
-        MH_Initialize();
         HMODULE shell32 = LoadLibrary((LPCWSTR) _T("shell32.dll"));
         void *SHGetKnownFolderPath = GetProcAddress(shell32, "SHGetKnownFolderPath");
-        MH_STATUS status = MH_CreateHook(SHGetKnownFolderPath, &HookSHGetKnownFolderPath,
-                                         (LPVOID *) &OriginalSHGetKnownFolderPath);
-        if (status == MH_OK) {
-            status = MH_EnableHook(SHGetKnownFolderPath);
-#ifdef HookDebug
-            if (status == MH_OK) {
-                MessageBox(NULL,  (LPCTSTR) _T("SHGetKnownFolderPath Hook Succeed"), (LPCTSTR) _T("SHGetKnownFolderPath Hook"), MB_OK);
-            } else {
-                MessageBox(NULL,  (LPCTSTR) _T("SHGetKnownFolderPath Hook Enable Fail"), (LPCTSTR) _T("SHGetKnownFolderPath Hook"), MB_OK);
-            }
-        } else {
-            MessageBox(NULL,  (LPCTSTR) _T("SHGetKnownFolderPath Hook Fail"), (LPCTSTR) _T("SHGetKnownFolderPath Hook"), MB_OK);
-#endif
-        }
-
+        MH_STATUS status = CreateMinHook(SHGetKnownFolderPath);
+        EnableMinHook(SHGetKnownFolderPath, status);
+        void *SHGetSpecialFolderPath = GetProcAddress(shell32, "SHGetKnownFolderPath");
+        status = CreateMinHook(SHGetSpecialFolderPath);
+        EnableMinHook(SHGetSpecialFolderPath, status);
     }
 }
 
