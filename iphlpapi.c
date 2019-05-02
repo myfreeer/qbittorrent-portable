d2775c68cc1add70c1f0e24b3db148e7f63bdb0e
myfreeer <myfreeer@users.noreply.github.com>
2019-05-02 22:31:10 +0800

    hijack: use detach callback
    probably safer after unload

--- iphlpapi.c
+++ iphlpapi.c
@@ -1,2 +1,3 @@
+#define DLL_HIJACK_DETACH_CALLBACK
 #include "dll-hijack/iphlpapi.h"
 #include "hook_knowndir.h"
--- version.c
+++ version.c
@@ -1,2 +1,3 @@
+#define DLL_HIJACK_DETACH_CALLBACK
 #include "dll-hijack/version.h"
 #include "hook_knowndir.h"
